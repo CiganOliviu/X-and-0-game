9344b6d06c1c407e3a102ba45e2d1832fc87fc0d
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-07 17:53:53 +0200

    Update gameEngine.cs

--- gameEngine/gameEngine/gameEngine.cs
+++ gameEngine/gameEngine/gameEngine.cs
@@ -27,9 +27,21 @@ using algo_data_structures;
 
 namespace gameEngine
 {
+    class validationRules
+    {
+        
+        public static bool isBiggerThanLength (int Value, int ToCompareValue)
+        {
+            if (Value >= ToCompareValue) return true;
+
+            return false;
+        } 
+    }
+
     class SetUpEnvironment
     {
-        public static void initialiseMatrix(ref matrixType<char> matrixObject)
+
+        public static void InitialiseMatrix(ref matrixType<char> matrixObject)
         {
              char initialisor = '0';
 
@@ -50,16 +62,33 @@ namespace gameEngine
             for (int iterator = matrixObject.StartLinePoint; iterator < matrixObject.Line; iterator++)
             {
                 for (int jiterator = matrixObject.StartColumnPoint; jiterator < matrixObject.Column; jiterator++)
-                    Console.Write(matrixObject.matrix[iterator, jiterator] + " ");
+                    Console.Write(matrixObject.matrix[iterator, jiterator] + "\t");
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
 
-        public static void MatrixInitialisation(ref matrixType<char> matrixObject, char Value, limits<int> position)
+        public static void MatrixItemInitialisation(ref matrixType<char> matrixObject, char Value, limits<int> position)
         {
+
+            if (validationRules.isBiggerThanLength(position.minimLimit, matrixObject.Line)) throw new System.ArgumentException("Cannot handle position bigger than line", "position.minimLimit");
+            if (validationRules.isBiggerThanLength(position.maximLimit, matrixObject.Line)) throw new System.ArgumentException("Cannot handle position bigger than line", "position.maximLimit");
+            if (validationRules.isBiggerThanLength(position.minimLimit, matrixObject.Column)) throw new System.ArgumentException("Cannot handle position bigger than column", "position.minimLimit");
+            if (validationRules.isBiggerThanLength(position.maximLimit, matrixObject.Column)) throw new System.ArgumentException("Cannot handle position bigger than column", "position.maximLimit");
+
+            if (matrixObject.matrix[position.minimLimit, position.maximLimit] != '0') throw new System.ArgumentException("Cannot assign an already assigned position on board", "matrixObject.matrix[position.minimLimit, position.maximLimit]");
+
             matrixObject.matrix[position.minimLimit, position.maximLimit] = Value;
         }
+    }
+
+    class GameEngine
+    {
+        private SetUpEnvironment setup = new SetUpEnvironment();
+        
+
     }
 
     class Program
@@ -68,10 +97,8 @@ namespace gameEngine
         {
             matrixType<char> GameBoard = new matrixType<char>();
 
-            SetUpEnvironment.initialiseMatrix(ref GameBoard);
-
+            SetUpEnvironment.InitialiseMatrix(ref GameBoard);
             SetUpEnvironment.PutsMatrix(ref GameBoard);
-
         }
     }
 }
